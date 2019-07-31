bac93a2e618ba55ecf724c1fc597549007cb2d35
Aisha Covington <atcovington1@gmail.com>
2019-07-31 15:36:54 -0400

    up to chapter6

--- my-angular-albums/src/app/albums/album-card/album-card.component.ts
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Album } from "../album.model";
+
 
 @Component({
-  selector: 'app-album-card',
-  templateUrl: './album-card.component.html',
-  styleUrls: ['./album-card.component.css']
+  selector: "app-album-card",
+  templateUrl: "./album-card.component.html",
+  styleUrls: ["./album-card.component.css"]
 })
 export class AlbumCardComponent implements OnInit {
+  @Input()
+  album: Album;
 
-  constructor() { }
+  @Output()
+    albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
 
-  ngOnInit() {
-  }
+  showAlbum() {
+    this.albumClicked.emit(this.album);
+}
 
+  constructor() {}
+
+  ngOnInit() {}
 }
--- my-angular-albums/src/app/app.module.ts
+++ my-angular-albums/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumListComponent } from './albums/album-list/album-list.component';
-import { AlbumCardComponent } from './albulms/album-card/album-card.component';
+import { AlbumCardComponent } from './albums/album-card/album-card.component';
 
 @NgModule({
   declarations: [
